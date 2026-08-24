031678ca45b85b495d1ad99986c07a8ebc0167a1
Elias <58146538+zafoddiy@users.noreply.github.com>
2026-08-24 16:50:03 +0300

    Remove introductory comments from simple_control.py
    Removed the initial comments describing the purpose and input/output of the script.

--- ias0220_231899/simple_control.py
+++ ias0220_231899/simple_control.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
 
 """
-Solution to home assignment 7 (Robot Control). Node to take a set of
-waypoints and to drive a differential drive robot through those waypoints
-using a simple PD controller and provided odometry data.
-
 @author: Elias Sepp
 @date: 06.12.24
-@input: Odometry as nav_msgs Odometry message
-@output: body velocity commands as geometry_msgs Twist message.
 """
 
 import math
